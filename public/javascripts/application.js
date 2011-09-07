29e21bffd8a285d1accdbf1d8c919c6320bdf53f
Prateek Narang <prateek.narang@gmail.com>
2011-09-07 13:19:23 +0530

    Fixing facebook login code

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,18 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+function initialize() {
+ $.getScript(document.location.protocol + '//connect.facebook.net/en_US/all.js');
+}
+
+window.fbAsyncInit = function() {
+ FB.init({
+  appId   : 110895615683537, // replaced here by my App ID
+  session : {},
+  status  : true,
+  cookie  : true,
+  xfbml   : true
+ });
+ FB.Event.subscribe('auth.login', function() {
+  window.location.href = 'about';
+ });
+};
